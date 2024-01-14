bc44c40e306346912c0d05d2a3fec1cbb2b36124
Masoomeh.Ebrahimzadeh <106168428+MasoomehE@users.noreply.github.com>
2024-01-14 20:08:40 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      vazir: ["vazir", "sans-serif"],
+      vazirBlack: ["vazirBlack", "sans-serif"],
+      vazirBold: ["vazirBold", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
